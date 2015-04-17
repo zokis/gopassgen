fb172e06d711c6f180dba77f5685ea045d783414
Marcelo Fonseca Tambalo <marcelo.zokis@gmail.com>
2015-04-17 19:13:08 -0300

    adiciona doc

--- gopassgen.go
+++ gopassgen.go
@@ -1,4 +1,4 @@
-package gopassgen
+package main
 
 import (
   "math/rand"
@@ -11,20 +11,24 @@ const (
   maxPasswordLength = 30
 )
 
+// StdChars default alphabet
 var StdChars = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$&(){}[]<>.:-_+=")
 
+// NPParams parameters
 type NPParams struct {
   chars  []rune
   length int
 }
 
-func OptionLenght(l int) func(p *NPParams) error {
+// OptionLength adds the option of password length
+func OptionLength(l int) func(p *NPParams) error {
   return func(p *NPParams) error {
     p.length = l
     return nil
   }
 }
 
+// OptionChars adds the alphabet option used
 func OptionChars(c []rune) func(p *NPParams) error {
   return func(p *NPParams) error {
     p.chars = c
@@ -37,6 +41,8 @@ func randInt(min int, max int) int {
   return min + rand.Intn(max - min)
 }
 
+
+// NewPassword generates a new password
 func NewPassword(options ...func(*NPParams) error) string {
 
     rand.Seed( time.Now().UTC().UnixNano())
