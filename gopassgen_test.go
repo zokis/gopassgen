3757d9e24276df37218cf3cc3466f77eaea6bfce
Marcelo Fonseca Tambalo <marcelo.zokis@gmail.com>
2015-04-22 18:13:38 -0300

    melhora testes

--- gopassgen_test.go
+++ gopassgen_test.go
@@ -8,12 +8,28 @@ type Test struct {
 }
 
 func TestLen(t *testing.T) {
-  t1 := Test{pass: NewPassword(), len_: 15}
-  t2 := Test{pass: NewPassword(OptionChars([]rune("ABCDEF."))),len_: 15}
-  t3 := Test{pass: NewPassword(OptionLength(16)),len_: 16}
-  t4 := Test{pass: NewPassword(OptionChars([]rune("abcdef.")), OptionLength(17)),len_: 17}
-  t5 := Test{pass: NewPassword(OptionLength(18), OptionChars([]rune("012345."))),len_: 18}
-  tests := []Test{t1, t2, t3, t4, t5}
+  tests := []Test{
+    Test{
+      pass: NewPassword(),
+      len_: 15,
+    },
+    Test{
+      pass: NewPassword(OptionChars([]rune("ABCDEF."))),
+      len_: 15,
+    },
+    Test{
+      pass: NewPassword(OptionLength(16)),
+      len_: 16,
+    },
+    Test{
+      pass: NewPassword(OptionChars([]rune("abcdef.")),OptionLength(17)),
+      len_: 17,
+    },
+    Test{
+      pass: NewPassword(OptionLength(18), OptionChars([]rune("012345."))),
+      len_: 18,
+    },
+  }
 
   for _, test := range tests {
     if test.len_ != len(test.pass){
