d47d78058cbba157746b6223ab7b55b854351295
Marcelo Fonseca Tambalo <marcelo.zokis@gmail.com>
2015-04-22 18:14:30 -0300

    melhora testes

--- gopassgen_test.go
+++ gopassgen_test.go
@@ -2,30 +2,30 @@ package gopassgen
 
 import "testing"
 
-type Test struct {
+type TestLen struct {
   pass string
   len_ int
 }
 
-func TestLen(t *testing.T) {
-  tests := []Test{
-    Test{
+func TestLength(t *testing.T) {
+  tests := []TestLen{
+    TestLen{
       pass: NewPassword(),
       len_: 15,
     },
-    Test{
+    TestLen{
       pass: NewPassword(OptionChars([]rune("ABCDEF."))),
       len_: 15,
     },
-    Test{
+    TestLen{
       pass: NewPassword(OptionLength(16)),
       len_: 16,
     },
-    Test{
+    TestLen{
       pass: NewPassword(OptionChars([]rune("abcdef.")),OptionLength(17)),
       len_: 17,
     },
-    Test{
+    TestLen{
       pass: NewPassword(OptionLength(18), OptionChars([]rune("012345."))),
       len_: 18,
     },
