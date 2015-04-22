14d9ab3d5e5aa37f510d38f5a8b4096ab148d22b
Marcelo Fonseca Tambalo <marcelo.zokis@gmail.com>
2015-04-22 18:26:58 -0300

    melhora testes aind anão funciona

--- gopassgen_test.go
+++ gopassgen_test.go
@@ -1,11 +1,18 @@
 package gopassgen
 
 import "testing"
+import "strings"
 
 type TestLen struct {
   pass string
   len_ int
 }
+
+type TestChar struct {
+  pass     string
+  alphabet []rune
+}
+
 
 func TestLength(t *testing.T) {
   tests := []TestLen{
@@ -36,4 +43,35 @@ func TestLength(t *testing.T) {
       t.Errorf("Len Error %s != %s", test.len_, len(test.pass))
     }
   }
+}
+
+func TestChars(t *testing.T) {
+  tests := []TestChar{
+    TestChar{
+      pass: NewPassword(),
+      alphabet: StdChars,
+    },
+    TestChar{
+      pass: NewPassword(OptionChars([]rune("ABCDEF."))),
+      alphabet: []rune("ABCDEF."),
+    },
+    TestChar{
+      pass: NewPassword(OptionChars([]rune("abcdef.")),OptionLength(17)),
+      alphabet: []rune("abcdef."),
+    },
+    TestChar{
+      pass: NewPassword(OptionLength(18), OptionChars([]rune("012345."))),
+      alphabet: []rune("12345."),
+    },
+  }
+
+  for _, test := range tests {
+    for _, c := range []byte(test.pass) {
+      
+      if strings.Contains(string(c), string(test.alphabet)) {
+        t.Errorf("%s not in %s", c, test.alphabet)
+      }
+    }
+  }
+
 }
